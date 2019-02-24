1b518b01a1dc14200273a33cdbc4c5da2e414fa3
traktor <v6234@abv.bg>
2019-02-24 19:13:53 +0200

    Added new functions,unfinished yet

--- Project 1 OOP TU-Varna/Project 1 OOP TU-Varna.cpp
+++ Project 1 OOP TU-Varna/Project 1 OOP TU-Varna.cpp
@@ -5,6 +5,7 @@
 #include<string>
 #include<vector>
 #include<list>
+#include<fstream>
 
 using namespace std;
 class Student
@@ -26,11 +27,11 @@ public:
 		disciplineName = inputDiscipline;
 		originalDisciplineName = disciplineName;
 	}
-	string GetFN()
+	string GetFN()const
 	{
 		return fn;
 	}
-	string GetDiscipline()
+	string GetDiscipline()const
 	{
 		return disciplineName;
 	}
@@ -42,7 +43,82 @@ public:
 	{
 		disciplineName = inputDiscipline;
 	}
-
+	static bool CompareStudent(const Student &inputStudent)
+	{
+		if (originalDisciplineName.compare(inputStudent.GetDiscipline())==0)
+		{
+			return 1;
+		}
+		return 0;
+	}
+	Student& Student::operator=(const Student& inputStudent)
+	{
+		fn = inputStudent.GetFN();
+		disciplineName = inputStudent.GetDiscipline();
+	}
+	bool operator<(const Student& inputStudent)
+	{
+		if (fn<inputStudent.GetFN()==true && disciplineName<inputStudent.GetDiscipline() == true)
+		{
+			return true;
+		}
+		return false;
+	}
+	friend istream& operator>>(istream &toStream,Student &inputStudent)
+	{
+		cout << "Input fn:" << endl;
+		toStream >> inputStudent.fn;
+		cout << "\nInput discipline" << endl;
+		toStream >> inputStudent.disciplineName;
+		return toStream;
+	}
+	friend ostream& operator<<(ostream &toStream,Student &inputStudent)
+	{
+		cout << "Name:" << endl;
+		toStream << inputStudent.fn;
+		cout << "\nDiscipline" << endl;
+		toStream << inputStudent.disciplineName;
+		return toStream;
+	}
+};
+class Discipline
+{
+private:
+	string semester;
+	vector<Student>a;
+	vector<Student>b;
+	vector<Student>c;
+	vector<Student>d;
+	list<Student>disciplines;
+	Discipline(string inputFile)
+	{
+		string name;
+		ifstream myfile(inputFile);
+		getline(myfile, name);
+		string data;
+		const string delim = " ";
+		string FN;
+		string DISCIPLINE;
+		int counter = 0;
+		try {
+			while (getline(myfile, data, ' '))
+			{
+				if (counter % 2 == 0) {
+					FN = data;
+				}
+				else {
+					DISCIPLINE = data;
+					Student zapis = Student(FN, DISCIPLINE);
+					disciplines.push_back(zapis);
+				}
+				counter++;
+			}
+		}
+		catch (ifstream::failure e) 
+		{
+			std::cerr << "Exception opening/reading/closing file\n";
+		}
+	}
 };
 
 int main()
